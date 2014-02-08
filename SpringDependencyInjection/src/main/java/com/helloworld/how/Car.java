8b22ef28b0bc115b58f0eb21e54b4dcddca50b46
Gelhrid <gelhrid@gmail.com>
2014-02-08 17:40:12 +0100

    Koniec

--- SpringDependencyInjection/src/main/java/com/helloworld/how/Car.java
+++ SpringDependencyInjection/src/main/java/com/helloworld/how/Car.java
@@ -1,14 +1,26 @@
 package com.helloworld.how;
 
 public class Car {
-private String type;
+private String types;
 private int id;
+private Color color;
+
+
+
+	public void setColor(Color color) {
+	this.color = color;
+}
 	public void drive(){
 		System.out.println("brum brum brum");
 	}
+	public Car(String types, int id) {
+	super();
+	this.types = types;
+	this.id = id;
+}
 	@Override
 	public String toString() {
-		return "Car [type=" + type + ", id=" + id + "]";
+		return "Car [types=" + types + ", id=" + id + ", color=" + color + "]";
 	}
 	
 	
--- SpringDependencyInjection/src/main/java/com/helloworld/how/Color.java
+++ SpringDependencyInjection/src/main/java/com/helloworld/how/Color.java
@@ -2,5 +2,17 @@ package com.helloworld.how;
 
 public class Color {
 	private String color;
+	private String special;
+
+	public Color(String color, String special) {
+		this.color = color;
+		this.special = special;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Color [color=" + color + ", special=" + special + "]";
+	}
 
 }
